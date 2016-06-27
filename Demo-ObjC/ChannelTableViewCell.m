b8d384358506c1627ed49ce31214821cdb6cc8d3
Randy Beiter <rbeiter@twilio.com>
2016-06-26 22:13:00 -0400

    Remove boiler-plate only code

--- Demo-ObjC/ChannelTableViewCell.m
+++ Demo-ObjC/ChannelTableViewCell.m
@@ -13,10 +13,6 @@
 
 @implementation ChannelTableViewCell
 
-- (void)awakeFromNib {
-    // Initialization code
-}
-
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
 
